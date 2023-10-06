f229867e8edbf37efc88c1924ae82d2d6c176b1e
奥山貴氏 <>
2023-10-06 10:57:01 +0900

    CMS直前まで

--- index.php
+++ index.php
@@ -332,7 +332,7 @@
                             <p>RAF</p>
                             17:00～24:00(L.O.23:00) ／ 第一,第三火曜定休日
                         </div>
-                    </dd>
+                    </dd>z
                 </dl>
 
                 <dl class="flex">
@@ -378,6 +378,104 @@
         <!-- //container_min -->
     </section>
     <!-- //requirements -->
+
+    <section id="brand">
+        <div class="brand_container">
+            <h2 class="sec_ttl">
+                BRANDS
+                <small>
+                店舗一覧
+                </small>
+            </h2>
+            <div class="flex">
+                <div class="brand_item">
+                    <div class="brand_thumb item1"></div>
+                    <div class="brand_info">
+                        <h3>
+                            <span>tote.</span>
+                            <small>トテ</small>
+                        </h3>
+                        <p>
+                            〒501-0026 岐阜県各務原市那加前野町4-175<br>
+                            tel.<a href="tel:0582609123">058-260-9123</a>
+                        </p>
+
+                        <div class="flex">
+                            <div>
+                                <img src="<?php echo get_template_directory_uri(); ?>/images/seat_icon.svg" alt="" class="">
+                                <span>12</span>
+                            </div>
+
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/brand_open.svg" alt="" class="">
+                        </div>
+
+                        <div class="cat">
+                            <span>美容室</span>
+                        </div>
+                    </div>
+                    <!-- //brand_info -->
+                </div>
+                <!-- //brand_item -->
+                
+                <div class="brand_item">
+                    <div class="brand_thumb item1"></div>
+                    <div class="brand_info">
+                        <h3>
+                            <span>tote.</span>
+                            <small>トテ</small>
+                        </h3>
+                        <p>
+                            〒501-0026 岐阜県各務原市那加前野町4-175<br>
+                            tel.<a href="tel:0582609123">058-260-9123</a>
+                        </p>
+
+                        <div class="flex">
+                            <div>
+                                <img src="<?php echo get_template_directory_uri(); ?>/images/seat_icon.svg" alt="" class="">
+                                <span>12</span>
+                            </div>
+
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/brand_open.svg" alt="" class="">
+                        </div>
+
+                        <div class="cat">
+                            <span>美容室</span>
+                        </div>
+                    </div>
+                    <!-- //brand_info -->
+                </div>
+                <!-- //brand_item -->
+                <div class="brand_item">
+                    <div class="brand_thumb item1"></div>
+                    <div class="brand_info">
+                        <h3>
+                            <span>tote.</span>
+                            <small>トテ</small>
+                        </h3>
+                        <p>
+                            〒501-0026 岐阜県各務原市那加前野町4-175<br>
+                            tel.<a href="tel:0582609123">058-260-9123</a>
+                        </p>
+
+                        <div class="flex">
+                            <div>
+                                <img src="<?php echo get_template_directory_uri(); ?>/images/seat_icon.svg" alt="" class="">
+                                <span>12</span>
+                            </div>
+
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/brand_open.svg" alt="" class="">
+                        </div>
+
+                        <div class="cat">
+                            <span>美容室</span>
+                        </div>
+                    </div>
+                    <!-- //brand_info -->
+                </div>
+                <!-- //brand_item -->
+            </div>
+        </div>
+    </section>
 
     <section id="entry">
         <div class="container">
